96d79e84d6b50c5032e81ef32e4bd95bf9de34c7
Albert <autelec@yandex.ru>
2024-08-01 02:18:17 +0400

    Добавил ветку девелопер в гит

--- tests/test_saver.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from src.vacancy import Vacancy
-
-def test_salary_comparison():
-    vacancy1 = Vacancy("Dev", "url", "50000 руб.", "Описание")
-    vacancy2 = Vacancy("Dev", "url", "100000 руб.", "Описание")
-    assert vacancy1 < vacancy2
-    assert not (vacancy1 == vacancy2)
-    assert vacancy2 > vacancy1
